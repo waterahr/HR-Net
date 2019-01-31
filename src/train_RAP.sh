80dfc5fd4338484a80769a687404dd776ac71fc6
anhaoran <anhaoran@localhost.localdomain>
2019-01-31 11:02:42 +0800

    update for RAP&PA-100K

--- src/train_RAP.sh
+++ src/train_RAP.sh
@@ -1,3 +1,5 @@
-python train_RAP.py -g 0 -c 51 -m GoogLeNet -b 64 -w ../models/imagenet_models/GoogLeNet_RAP/binary51_final_model.h5
-python train_RAP_hiarchical.py -m hiarBayesGoogLeNet -b 64 -g 0 -c 51 -w ../models/imagenet_models/hiarBayesGoogLeNet_RAP/binary51_final_model.h5
-python train_RAP_hiarchical.py -m hiarGoogLeNet -b 64 -g 0 -c 51 -w ../models/imagenet_models/hiarGoogLeNet_RAP/binary51_final_model.h5
+python train_RAP.py -m GoogLeNet -c 51 -b 64 -g 1 -p 0
+python train_RAP.py -m GoogLeNet -c 51 -b 64 -g 1 -p 1
+python train_RAP.py -m GoogLeNet -c 51 -b 64 -g 1 -p 2
+python train_RAP.py -m GoogLeNet -c 51 -b 64 -g 1 -p 3
+python train_RAP.py -m GoogLeNet -c 51 -b 64 -g 1 -p 4
