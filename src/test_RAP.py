af27c42872fc696bc7469851b1f9750eca3ad758
waterahr <waterahr@gmail.com>
2019-03-13 10:49:39 +0800

    20190313 commit

--- src/test_RAP.py
+++ src/test_RAP.py
@@ -22,7 +22,22 @@ import tqdm
 from keras import backend as K
 from angular_losses import bayes_binary_crossentropy
 
-alpha = []
+def generate_imgdata_from_file(X_path, y, batch_size, image_height, image_width):
+    while True:
+        cnt = 0
+        X = []
+        Y = []
+        for i in range(len(X_path)):
+            img = image.load_img(X_path[i], target_size=(image_height, image_width, 3))
+            img = image.img_to_array(img)
+            X.append(img)
+            Y.append(y[i])
+            cnt += 1
+            if cnt==batch_size:
+                cnt = 0
+                yield (np.array(X), np.array(Y))
+                X = []
+                Y = []
 
 def parse_arg():
     models = ['GoogLeNet', 'Inception_v4', 'GoogLeNetGAP']
@@ -31,7 +46,7 @@ def parse_arg():
                         help='The gpu device\'s ID need to be used')
     parser.add_argument('-c', '--classes', type=int, default=92,
                         help='The total number of classes to be predicted')
-    parser.add_argument('-b', '--batch', type=int, default=64,
+    parser.add_argument('-b', '--batch', type=int, default=1,
                         help='The batch size of the training process')
     parser.add_argument('-wd', '--width', type=int, default=120,
                         help='The width of thWPAL_PETAe picture')
@@ -41,6 +56,8 @@ def parse_arg():
                         help='The weights file of the pre-training')
     parser.add_argument('-m', '--model', type=str, default='',
                         help='The model including: '+str(models))
+    parser.add_argument('-s', '--split', type=int, default=0,
+                        help='The split')
     args = parser.parse_args()
     os.environ['CUDA_VISIBLE_DEVICES'] = args.gpus
     return args
@@ -92,24 +109,32 @@ if __name__ == "__main__":
     data = np.array(pd.read_csv(filename))[:, 1:]
     length = len(data)
     #global alpha
-    data_x = np.zeros((length, image_height, image_width, 3))
+    load = False
+    if load:
+        data_x = np.zeros((length, image_height, image_width, 3))
     data_y = np.zeros((length, class_num))
+    data_path = []
     for i in range(length):
         #img = image.load_img(path + m)
-        img = image.load_img(data[i, 0], target_size=(image_height, image_width, 3))
-        data_x[i] = image.img_to_array(img)
+        data_path.append(data[i, 0])
+        if load:
+            img = image.load_img(data[i, 0], target_size=(image_height, image_width, 3))
+            data_x[i] = image.img_to_array(img)
         data_y[i] = np.array(data[i, 1:1+class_num], dtype="float32")
+    data_path = np.array(data_path)
     #data_y = data_y[:, part]
     #class_num = 3
     #X_train, X_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.3, random_state=0)
     #"""
     split = np.load('../results/RAP_partion.npy').item()
-    X_test = data_x[list(split['test'][0])]
+    if load:
+        X_test = data_x[list(split['test'][args.split])]
+    X_test_path = data_path[list(split['test'][args.split])]
     y_test = data_y[list(split['test'][0])]
     #"""
     #X_test = data_x[33268:]
     #y_test = data_y[33268:]
-    print("The shape of the X_test is: ", X_test.shape)
+    print("The shape of the X_test is: ", X_test_path.shape)
     print("The shape of the y_test is: ", y_test.shape)
     
     
@@ -121,7 +146,7 @@ if __name__ == "__main__":
         loss_weights = None
         metrics=['accuracy']
     elif args.model == "GoogLeNetGAP":
-        model_dir = "GoogLeNetGAP_RAP"
+        model_dir = "GoogLeNetGAP_RAP/"
         model = GoogLeNetGAP.build(image_height, image_width, 3, class_num)
         loss_func = 'binary_crossentropy'
         loss_weights = None
@@ -148,14 +173,19 @@ if __name__ == "__main__":
     model.compile(loss=loss_func, optimizer='adam', loss_weights=loss_weights, metrics=metrics)
     model.summary()
 
-    reg = args.weight + "_(e|f)1*"
+    reg = args.weight + "_(e|f)1*"#
     print(reg)
     weights = [s for s in os.listdir("../models/imagenet_models/" + model_dir) 
           if re.match(reg, s)]
     print(weights)
+    batch_size = args.batch
+    if load:
+        test_generator = datagen.flow(X_test, y_test, batch_size=batch_size)
+    else:
+        test_generator = generate_imgdata_from_file(X_test_path, y_test, batch_size, image_height, image_width)
     for w in tqdm.tqdm(weights):
         model.load_weights("../models/imagenet_models/" + model_dir + w, by_name=True)
-        predictions_list = model.predict(X_test)  
+        predictions_list = model.predict_generator(test_generator, steps=y_test.shape[0]/batch_size)
         if args.model == "GoogLeNetv2":
             predictions = np.array(predictions_list).reshape((class_num, -1)).T
         else:
